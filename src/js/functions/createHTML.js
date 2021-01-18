703544fe3513f9c0d15d836208b0385a4bdbec9c
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2021-01-18 17:34:58 +0400

    adding trash / check events

--- src/js/functions/createHTML.js
+++ src/js/functions/createHTML.js
@@ -1,16 +1,20 @@
 import {itemsList} from "../var";
+import {check, trash} from "./events";
 
 export function createHTML(todo) {
-    if (!todo.todo){
+    if (!todo.todo) {
         return
     }
     const html = `
             <span>${todo.todo}</span>
             <button name="trash" class="trash">🗑️</button>
-            <button class="check">✔️</button>
+            <button name="check" class="check">✔️</button>
     `
     const li = document.createElement("li");
     li.classList.add('item');
     li.innerHTML = html;
     itemsList.insertBefore(li, itemsList.firstChild);
+
+    li.children.trash.onclick = trash;
+    li.children.check.onclick = check;
 }
