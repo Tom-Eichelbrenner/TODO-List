e661a12ba5e00884a52af8d646a5d46c133023ef
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2021-01-18 17:15:42 +0400

    fixing the fact that input wernt working anymore because of object.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import './css/style.css';
 import './css/normalize.css';
 import {createItem} from "./js/functions/createItem";
 import {todoForm} from "./js/var";
-import {loadHTML} from "./loadHTML";
+import {loadHTML} from "./js/functions/loadHTML";
 
-function main(){
+function main() {
     window.addEventListener("load", loadHTML)
     todoForm.addEventListener("submit", createItem);
 }
+
 main();
+
